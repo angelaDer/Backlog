da817464b64156c521595daf9ad20e1f83a24a00
Angela <angela.derewienczuk@gmail.com>
2018-04-18 23:04:11 +0200

    Dodanie usuwania backlogu /Angela

---
+++ Desktop/ScrumMajster-master/app/src/main/java/com/ciastkaipiwo/android/scrummajster/BacklogActivity.java
@@ -0,0 +1,76 @@
+package com.ciastkaipiwo.android.scrummajster;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BacklogActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_ADD_TASK = 1;
+    private FloatingActionButton mPlus;
+
+    private List<Task> NameList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private TasksAdapter mTasksAdapter;
+
+    public void onResume() {
+        super.onResume();
+        mRecyclerView.setAdapter(mTasksAdapter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backlog);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.backlog_recycler_view);
+        mTasksAdapter = new TasksAdapter(NameList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mTasksAdapter);
+
+
+        mPlus = (FloatingActionButton) findViewById(R.id.add_task);
+        mPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BacklogActivity.this, TaskConfigActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_ADD_TASK);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_ADD_TASK) {
+            if (data == null) {
+                return;
+            }
+            System.out.println(NameList.size());
+            NameList.add(TaskConfigActivity.getNewTask(data));
+            System.out.println(NameList.size());
+            Toast.makeText(BacklogActivity.this, "Pomyslnie dodano task: "+String.valueOf(TaskConfigActivity.getNewTask(data).getTime()), Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ Desktop/ScrumMajster-master/app/src/main/java/com/ciastkaipiwo/android/scrummajster/TasksAdapter.java
@@ -0,0 +1,73 @@
+package com.ciastkaipiwo.android.scrummajster;
+
+/**
+ * Created by Klaudia on 12.04.2018.
+ */
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TaskViewHolder>{
+
+    private List<Task> tasksList;
+
+    public TasksAdapter(List<Task> tasksList) {
+        this.tasksList = tasksList;
+    }
+
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.tasks_list_row, parent, false);
+        return new TaskViewHolder(itemView, tasksList);
+    }
+
+    @Override
+    public void onBindViewHolder(TaskViewHolder holder, int position) {
+        holder.name.setText(tasksList.get(position).getStory());
+        holder.weight.setText("Weight: "+String.valueOf(tasksList.get(position).getWeight()));
+        holder.time.setText("Time: "+String.valueOf(tasksList.get(position).getTime()));
+        holder.position = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasksList.size();
+    }
+
+    public class TaskViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView name;
+        public TextView weight;
+        public TextView time;
+        public int position;
+        private ArrayList<Task> taskList;
+
+
+        public TaskViewHolder(View view, List<Task>taskList) {
+            super(view);
+            view.setOnClickListener(this);
+            this.taskList = (ArrayList<Task>) taskList;
+            name = (TextView) view.findViewById(R.id.task_row_story);
+            weight = (TextView) view.findViewById(R.id.task_row_weight);
+            time = (TextView) view.findViewById(R.id.task_row_time);
+        }
+        @Override
+        public void onClick(View v){
+            Intent intent = TaskActivity.newIntent(v.getContext(), tasksList.get(this.position));
+            intent.putExtra("taskList",this.taskList);
+            intent.putExtra("position",position);
+            v.getContext().startActivity(intent);
+        }
+    }
+
+}
